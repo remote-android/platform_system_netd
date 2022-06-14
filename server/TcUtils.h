56ac72641c7bb335b90979e6523da89c21792af6
Maciej Żenczykowski <maze@google.com>
2022-06-13 22:39:12 -0700

    TcUtils.h - remove most of it - no longer used
    Bug: 235778483
    Test: TreeHugger
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: I515f5c25d7ef6d0aeed091852e912fc2a334d548
    (cherry picked from commit da278449384fbe0ea898ff09cc90f0150d31d1a8)
    Merged-In: I515f5c25d7ef6d0aeed091852e912fc2a334d548

--- server/TcUtils.h
+++ server/TcUtils.h
@@ -26,41 +26,9 @@
 #include <string>
 
 #include "bpf/BpfUtils.h"
-#include "bpf_shared.h"
 
 namespace android {
 namespace net {
-
-// For better code clarity - do not change values - used for booleans like
-// with_ethernet_header or isEthernet.
-constexpr bool RAWIP = false;
-constexpr bool ETHER = true;
-
-// For better code clarity when used for 'bool ingress' parameter.
-constexpr bool EGRESS = false;
-constexpr bool INGRESS = true;
-
-// The priority of clat hook - must be after tethering.
-constexpr uint16_t PRIO_CLAT = 4;
-
-inline base::Result<bool> isEthernet(const std::string& interface) {
-    bool result = false;
-    if (int error = ::android::isEthernet(interface.c_str(), result)) {
-        errno = error;
-        return ErrnoErrorf("isEthernet failed for interface {}", interface);
-    }
-    return result;
-}
-
-inline int getClatEgress4MapFd(void) {
-    const int fd = bpf::mapRetrieveRW(CLAT_EGRESS4_MAP_PATH);
-    return (fd == -1) ? -errno : fd;
-}
-
-inline int getClatIngress6MapFd(void) {
-    const int fd = bpf::mapRetrieveRW(CLAT_INGRESS6_MAP_PATH);
-    return (fd == -1) ? -errno : fd;
-}
 
 inline int tcQdiscAddDevClsact(int ifIndex) {
     return doTcQdiscClsact(ifIndex, RTM_NEWQDISC, NLM_F_EXCL | NLM_F_CREATE);
@@ -72,26 +40,6 @@ inline int tcQdiscReplaceDevClsact(int ifIndex) {
 
 inline int tcQdiscDelDevClsact(int ifIndex) {
     return doTcQdiscClsact(ifIndex, RTM_DELQDISC, 0);
-}
-
-// tc filter add dev .. ingress prio 4 protocol ipv6 bpf object-pinned /sys/fs/bpf/... direct-action
-inline int tcFilterAddDevIngressClatIpv6(int ifIndex, const std::string& bpfProgPath) {
-    return tcAddBpfFilter(ifIndex, INGRESS, PRIO_CLAT, ETH_P_IPV6, bpfProgPath.c_str());
-}
-
-// tc filter add dev .. egress prio 4 protocol ip bpf object-pinned /sys/fs/bpf/... direct-action
-inline int tcFilterAddDevEgressClatIpv4(int ifIndex, const std::string& bpfProgPath) {
-    return tcAddBpfFilter(ifIndex, EGRESS, PRIO_CLAT, ETH_P_IP, bpfProgPath.c_str());
-}
-
-// tc filter del dev .. ingress prio 4 protocol ipv6
-inline int tcFilterDelDevIngressClatIpv6(int ifIndex) {
-    return tcDeleteFilter(ifIndex, INGRESS, PRIO_CLAT, ETH_P_IPV6);
-}
-
-// tc filter del dev .. egress prio 4 protocol ip
-inline int tcFilterDelDevEgressClatIpv4(int ifIndex) {
-    return tcDeleteFilter(ifIndex, EGRESS, PRIO_CLAT, ETH_P_IP);
 }
 
 }  // namespace net
